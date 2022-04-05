0f16a0ff133b09845776150867afbaaaa59ca447
DaoNik <nikitataranin@gmail.com>
2022-04-05 19:52:36 +0300

    feed: создал простой проект и добавил локализацию

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-books-market';
+  company: string = 'Lokalise';
+  created_by: string = $localize`Created by ${this.company}`;
+  tasksCount: number = 34;
+
+  localesList: any[] = [
+    { code: 'en-US', label: 'English' },
+    { code: 'ru', label: 'Русский' }
+  ]
+
+  genderCode: number = 0;
+
+  today: number = Date.now()
+
+  male() { this.genderCode = 0 }
+  female() { this.genderCode = 1 }
+  other() { this.genderCode = 2 }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {registerLocaleData} from "@angular/common";
+import localeRu from '@angular/common/locales/ru'
+
+registerLocaleData(localeRu, 'ru')
 
 @NgModule({
   declarations: [
